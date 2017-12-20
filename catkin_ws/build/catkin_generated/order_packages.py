74dc4259e80cd91d582fc68a6295150ef905b5e9
Thurden <huangshuaivf@gmail.com>
2017-12-20 20:25:44 +0900

    ROS WORKSPACE

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/huangshuai/catkin_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/huangshuai/turtlebot/devel;/home/huangshuai/kobuki/devel;/home/huangshuai/rocon/devel;/opt/ros/indigo".split(';') if "/home/huangshuai/turtlebot/devel;/home/huangshuai/kobuki/devel;/home/huangshuai/rocon/devel;/opt/ros/indigo" != "" else []
